cac1ce001fc594ec29503447061ee8dfbde07c3f
randomnerd41 <classiccatlinux@gmail.com>
2026-01-27 16:19:21 -0500

    Update nerdMOD.py
    now works with arch linux!
    small update

--- nerdMOD.py
+++ nerdMOD.py
@@ -1,4 +1,5 @@
 # == nerdMOD - 1.0_alpha ==
+# a python module made by randomnerd41
 
 import os
 import sys
@@ -28,4 +29,21 @@ def update_debian():
         time.sleep(3)
         clear()
         
+def update_arch():
+    yee = input("do you 100% want to update your system? (y/n) ")
+    if yes = "y":
+        os.system("sudo pacman -Syu")
+    else:
+        print("will not update")
+        time.sleep(3)
+        clear()
         
+def update_arch_yay():
+    ehh = input("do you 100% want to update your systen with yay? (y/n) ")
+    if ehh = "y":
+        os.system('yay -Syu')
+    else:
+        print("will not update")
+        time.sleep(3)
+        clear()
+
